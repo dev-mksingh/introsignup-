14fe4334452797bb92ce681cf93bd55a0aee5c4e
dev-mksingh <108165255+dev-mksingh@users.noreply.github.com>
2022-12-09 13:54:06 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+const myform = document.querySelector('form');
+myform.reset();
+const fname = document.querySelector('#f_name');
+const lname = document.querySelector('#l_name');
+const email = document.querySelector('#email');
+const password = document.querySelector('#password');
+const submit = document.querySelector('#submit');
+const allElements = document.querySelectorAll('input:not(:last-child)');
+console.log(allElements);
+
+const errors = document.querySelectorAll('.error');
+console.log(errors);
+
+submit.addEventListener('click', function(evt){
+  evt.preventDefault();
+  for(var i = 0; i < allElements.length; i++) {
+    if(allElements[i].id==="email" && (allElements[i].value.match(/^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/!==true)|| allElements[i].value==='')) {
+        errors[i].textContent=`Looks like this is not an email`;
+    }
+    if(allElements[i].value==='' && allElements[i].id!=="email") {
+        errors[i].textContent=`${allElements[i].name} can't be empty`;
+    }
+  }
+})
